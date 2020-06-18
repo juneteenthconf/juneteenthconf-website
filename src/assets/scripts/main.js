d1426940cab41a476b21aac5c45834a6fc5a0b5d
Narmatha <Narmatha.Balasundaram@microsoft.com>
2020-06-18 06:30:15 -0400

    Supporting speakers page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,6 +2,7 @@ import schedule from './modules/schedule';
 import tracks from './modules/tracks';
 import persons from './modules/persons';
 import speakers from './modules/speakers';
+import stalks from './modules/stalks';
 
 import 'bootstrap/js/src/util';
 import 'bootstrap/js/src/modal';
@@ -15,4 +16,5 @@ document.addEventListener('DOMContentLoaded', () => {
   tracks.init();
   persons.init();
   speakers.init();
+  stalks.init();
 });
